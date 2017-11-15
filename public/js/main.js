38c5a72724811a502220d34de5c8f0a500babfce
Michael  Boyer <michaelboyer@Michaels-MacBook-Pro.local>
2017-11-15 13:49:39 -0700

    pretty

--- public/js/main.js
+++ public/js/main.js
@@ -25,20 +25,24 @@ var mainVm = new Vue({
 
         details:[],
     },
+    mounted: function() {
+        $.get('/locations', (dataFromServer) => {
+            this.locations = dataFromServer
+        })
     methods: {
-        findHostels: function(event){
-            event.preventDefault();
+        // findHostels: function(event){
+        //     event.preventDefault();
             
-            $.post('/hosteldata', {Object: object}, function(data){ //data sent must be an object, placeholder object inserted
+        //     $.post('/hosteldata', {Object: object}, function(data){ //data sent must be an object, placeholder object inserted
                 
-            })
-        },
-        findAirbnbs: function(event){
-            event.preventDefault();
+        //     })
+        // },
+        // findAirbnbs: function(event){
+        //     event.preventDefault();
             
-            $.post('/airbnbdata', {Object: object}, function(data){ //data sent must be an object, placeholder object inserted
+            // $.post('/airbnbdata', {Object: object}, function(data){ //data sent must be an object, placeholder object inserted
                 
-            })
+            // })
         },
         findHotels: function(event){
             event.preventDefault();
@@ -62,6 +66,22 @@ var mainVm = new Vue({
         
         },
 
+
+        findHotelDetails: function(location_id, event){
+            event.preventDefault();
+            event.stopPropagation();
+            // id = place_id
+            // console.log(event.target.id)
+            
+            $.post('/hoteldetails', {hotelObject: location_id}, function(data){
+            console.log("Event Target ID: ", location_id)
+
+            hotelDetails = JSON.parse(data)
+            console.log(hotelDetails)
+            mainVm.locations.push(hotelDetails.result)
+            console.log("MainVM details: ", mainVm.details)
+
+
         findHotelDetails: function(location, event){
             event.preventDefault();
             if ( location.details ) {
@@ -81,6 +101,7 @@ var mainVm = new Vue({
                 mainVm.$forceUpdate()
                 console.log("MainVM details: ", mainVm.details)
             
+
             })
         },
 
--- server.js
+++ server.js
@@ -2,25 +2,36 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 var app = express()
 
-mongoose.connect('mongodb://localhost/travel-project');
+mongoose.connect('mongodb://localhost/travel-project')
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 
 app.use(express.static('./public'))
+
+var hotelSchema = new mongoose.Schema({
+    name: {type: String, required: true}
+    price: {type: Number, required: true}
+    details: {type: Array}
+
+})
+
+var hotelModel = mongoose.model('hotel', hotelSchema)
+
+
 
 app.get('/', function(req, res){
     res.sendFile('./html/index.html', {root: './public'})
 })
 
-app.post('/hosteldata', function(req, res) {
-})
+// app.post('/hosteldata', function(req, res) {
+// })
 
-app.post('/airbnbdata', function(req, res) {
-})
+// app.post('/airbnbdata', function(req, res) {
+// })
 
 
 app.post('/hoteldata', function(req, res) {
@@ -42,37 +53,12 @@ app.post('/hoteldetails', function(req, res) {
     })
 })
 
-app.post('/hotelPrices', function(req, res) {
-    request(`https://www.googleapis.com/travelpartner/v2.0/e4fb96365841de2c3b1d83ca2e7f28933a3e1866/prices/${req.body.hotelObject}`, function (error, response, body) {
-    console.log('data from google: ', body)
-    res.send(body)
-
-    
-    })
-})
-
-// Places API
-// app.get('/api', function(req, res){
-//     console.log('pinging', req.query)
-
-//     //parameters: query, type=lodging, radius=10000 meters(we can have user choose radius)
-
-
-// })
-// // Place details API
-// app.get('/api', function(req, res){
-//     console.log('pinging', req.query)
-
-    //parameters: placeid
-
-
-// })
 
 
 
 
 app.listen(8080, function() {
-    console.log('started on 8080');
+    console.log('started on 8080')
 })
 
 
